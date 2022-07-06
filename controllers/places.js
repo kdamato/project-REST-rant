9006b063a4b6f722824a0ca42915113693cbf62b
Kyle DAmato <kyledamato4@gmail.com>
2022-07-06 13:45:21 -0400

    fixed places page

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
 
 router.get('/', function(req, res){
-    res.send('GET /places')
+    let places = [{
+                name: 'Rook Coffee',
+                city: 'Long Branch',
+                state: 'NJ',
+                cuisines: 'Coffee, Bakery',
+                pic: 'https://cdn.shopify.com/s/files/1/1978/4569/files/RookLogoFinished_180x_1be85ac5-b282-483f-baee-31eaff1c3a55_174x.png?v=1519333525'
+              }, {
+                name: 'In-N-Out Burger',
+                city: 'Los Angeles',
+                state: 'CA',
+                cuisines: 'Burgers, Snacks',
+                pic: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/InNOut_2021_logo.svg/1200px-InNOut_2021_logo.svg.png'
+              }]
+    res.render('places/index',{places})
 })
 
 module.exports = router
