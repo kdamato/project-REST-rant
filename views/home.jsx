b55cac584dfd67e6a2c449231c98e9805db4b441
Kyle DAmato <kyledamato4@gmail.com>
2022-07-08 19:44:29 -0400

    adding pictures

--- views/home.jsx
+++ views/home.jsx
@@ -4,9 +4,19 @@ const Def = require('./default')
 function home(){
     return (
         <Def>
+            
             <main>
                 <h1>HOME</h1>
-            </main>
+                <div>
+                    <img src="public/css/images/Cheesecake.jpg" alt="Cheesecake"></img>
+                    <div>
+                        Photo by <a href='https://unsplash.com/@anna_tukhfatullina'>Anna Tukhfatullina</a>
+                    </div>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                    </a>
+                </main>
         </Def>
     )
 }
