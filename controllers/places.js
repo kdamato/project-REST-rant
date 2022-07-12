aa7346765a4071760e0d8a6fc2c6985b00271344
Kyle DAmato <kyledamato4@gmail.com>
2022-07-12 17:38:16 -0400

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -13,21 +13,30 @@ router.get('*', function (req,res){
   res.render('error404')
 })
 
-router.get('/:id', function(req,res){
-  res.send('places/:id')
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {place: places[id]})
+  }
 })
 
 router.post('/', (req, res) => {
-  // if (!req.body.pic) {
-  //   // Default image if one is not provided
-  //   req.body.pic = 'public/css/images/Burger.jpg'
-  // }
-  // if (!req.body.city) {
-  //   req.body.city = 'Anytown'
-  // }
-  // if (!req.body.state) {
-  //   req.body.state = 'USA'
-  // }
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'https://assets.epicurious.com/photos/5c745a108918ee7ab68daf79/5:4/w_3129,h_2503,c_limit/Smashburger-recipe-120219.jpg'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
   places.push(req.body)
   res.redirect('/places')
 })
