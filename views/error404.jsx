329d81906f2bd376a6345e7b62dbb346a85c132b
Kyle DAmato <kyledamato4@gmail.com>
2022-07-12 17:28:12 -0400

    Navbar with style

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,7 +8,7 @@ function error404(){
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry we can't find this page!</p>
-                <img src='public/css/images/puppies.jpg' alt='puppy'/>
+                <img src='https://media.istockphoto.com/photos/labrador-puppy-in-glasses-with-coffee-picture-id1248734146?k=20&m=1248734146&s=170667a&w=0&h=XGf0Np8Sb8pRiMX5ia094uysSm7ZDeUsL4NWSbN9KbE=' alt='puppy'/>
             </main>
         </Def>
     )
