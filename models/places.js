19ff2bb1411c4474dabb1e4164d0b0ad77faf346
Kyle DAmato <kyledamato4@gmail.com>
2022-07-16 02:34:51 -0400

    fixed defaults

--- models/places.js
+++ models/places.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  pic: String,
+  pic: {type: String, default: 'https://www.eatthis.com/wp-content/uploads/sites/4/2022/04/epic-burger-cheeseburger.jpg?quality=82&strip=1'},
   cuisines: { type: String, required: true },
   city: { type: String, default: 'Anytown' },
   state: { type: String, default: 'USA' },
