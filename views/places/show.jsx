294a110e38ec2c133275cc77d9973fba9b8d87c5
Kyle DAmato <kyledamato4@gmail.com>
2022-07-23 12:19:27 -0400

    created star rating functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,17 @@ function show (data) {
             No comments yet!
           </h3>
         )
+        let rating = (
+            <h3 className='inactive'>Not yet rated</h3>
+        )
         if (data.place.comments.length) {
+            let sumRatings = data.place.comments.reduce((tot,c) => {
+                return tot + c.stars
+            }, 0)
+            let averageRating = sumRatings / data.place.comments.length
+            rating = (
+                <h3>{Math.round(averageRating)} stars</h3>
+            )
           comments = data.place.comments.map(c => {
             return (
               <div className="border">
@@ -34,7 +44,7 @@ function show (data) {
                 </div>
                 <div className="col-sm-6">
                 <h2>Ratings</h2>
-                <p>currently unrated</p>
+                {rating}
                 <h2>
                     Description
                 </h2>
